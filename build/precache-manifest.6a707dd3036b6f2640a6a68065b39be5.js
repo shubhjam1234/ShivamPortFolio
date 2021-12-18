d57fca1b1e5640a754b1c9b73224cceed6bdd88f
Amey <bamey2241997@gmail.com>
2021-12-18 23:02:13 +0530

    Added changes for the dadu's website

---
+++ build/precache-manifest.6a707dd3036b6f2640a6a68065b39be5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d3b4715f17fba74a5c9f47442c6e88",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "8077b0d97c86d99bfe7e",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "ea4c23db1757a4f0b504",
+    "url": "/home/static/js/2.c6a95f53.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.c6a95f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8077b0d97c86d99bfe7e",
+    "url": "/home/static/js/main.1ae27481.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "702b8bc39a01c74680883c3e6e741199",
+    "url": "/home/static/media/prassanna.702b8bc3.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const Footer = (props) => {
+  const bgStyle = { backgroundColor: "#f5f5f5" };
+
+  return (
+    <footer style={bgStyle} className="mt-auto py-5 text-center ">
+      <Container>
+        {props.children}
+        Made with ❤️ by{" "}
+        <a
+          className="badge badge-dark"
+          rel="noopener"
+          href="https://github.com/ameygoes"
+          aria-label="My GitHub"
+        >
+          Amey Bhilegaonkar
+        </a>{" "}
+        using <i className="fab fa-react" />
+        <p>
+          <small className="text-muted">
+            Project code is open source. Feel free to fork and make your own
+            version.
+          </small>
+        </p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
